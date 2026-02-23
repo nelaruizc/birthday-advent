15901c5c10dff40ec928f4631882e94e25e3c526
Nelanyi Ruiz <nelaruizc@gmail.com>
2026-02-23 12:27:29 +0100

    Use public birthday cake SVG with BASE_URL in hero

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,22 @@ const Home = ({
       openedGifts={openedGifts}
       isUnlocked={(id) => isGiftUnlocked(id, new Date())}
     />
-    <footer className="pb-8 pt-1 text-center text-sm text-blue-100/80">Made with love, by Nela</footer>
+    <footer className="flex items-center justify-center gap-1 pb-8 pt-1 text-center text-sm text-blue-100/80">
+      <span>Made with</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="icon icon-tabler icons-tabler-filled icon-tabler-heart"
+        aria-label="heart icon"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
+      </svg>
+      <span>, by Nela.</span>
+    </footer>
   </>
 )
 
